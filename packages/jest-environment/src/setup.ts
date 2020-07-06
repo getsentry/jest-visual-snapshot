aa82835a2bd226394137de02582f9a18e6b5cadd
Billy Vong <billy@sentry.io>
2020-07-05 23:21:44 -0700

    fix: make sure output dir exists

--- packages/jest-environment/src/setup.ts
+++ packages/jest-environment/src/setup.ts
@@ -1,8 +1,14 @@
+import { promises as fs } from "fs";
+
 import { getBrowser } from "./browser";
 import type { VisualSnapshotConfig } from "./Environment";
 
-export default function visualSnapshotGlobalSetup(
+export default async function visualSnapshotGlobalSetup(
   config?: VisualSnapshotConfig
 ) {
   getBrowser();
+
+  if (config?.output) {
+    await fs.mkdir(config.output);
+  }
 }

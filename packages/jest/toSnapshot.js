715384a9e6827884c7bf34fa2a42e113d0427a92
Billy Vong <billyvg@users.noreply.github.com>
2021-06-21 16:04:05 -0700

    feat: Upgrade @jest/environment to v26 (#16)
    * remove babel
    * upgrade jest to 26
    * remove babel-eslint
    * remove babelrc
    * update toSnapshot for Testing Library

--- packages/jest/toSnapshot.js
+++ packages/jest/toSnapshot.js
@@ -1,24 +1,38 @@
+function getHtml(node) {
+  if (typeof node.html === "function") {
+    return node.html();
+  }
+
+  if (typeof node.innerHTML !== "undefined") {
+    return node.innerHTML;
+  }
+
+  throw new Error(
+    "Unable to snapshot object, must be able to be rendered in jsdom"
+  );
+}
+
 function serializeMultipleNodes(nodes) {
-  return nodes.map((node) => node.html()).join("\n");
+  return nodes.map(getHtml).join("\n");
 }
 
 function toSnapshot(received, args) {
-  if (typeof received.html !== "function") {
+  try {
+    return global.addSnapshot({
+      html:
+        received.length > 1
+          ? `<div>${serializeMultipleNodes(received)}</div>`
+          : getHtml(received),
+      testName: this.currentTestName,
+      args,
+    });
+  } catch {
     return {
       message: () =>
         "Unable to snapshot object, must be able to be rendered in jsdom",
       pass: false,
     };
   }
-
-  return global.addSnapshot({
-    html:
-      received.length > 1
-        ? `<div>${serializeMultipleNodes(received)}</div>`
-        : received.html(),
-    testName: this.currentTestName,
-    args,
-  });
 }
 
 module.exports = toSnapshot;
